eb03c269388928145d0efdb61c52dc850bdf7de5
Simeon Ackermann <amseon@web.de>
2015-09-07 17:36:27 +0200

    Make OntoWiki accessable under /ontowiki

--- ontowiki-docker/start.sh
+++ ontowiki-docker/start.sh
@@ -4,7 +4,7 @@
 chmod +x /config/config-run.sh
 /config/config-run.sh
 
-CONFFILE=/var/www/config.ini
+CONFFILE=/var/www/ontowiki/config.ini
 
 # set Virtuoso password in ontowikis config.ini
 sed -i "s/\(store.virtuoso.password\s*\)= \"dba\"$/\1= \"${STORE_ENV_PWDDBA}\"/" ${CONFFILE}
@@ -22,7 +22,7 @@ cat /ow-docker.fig
 echo ""
 echo "following log:"
 
-OWLOG="/var/www/logs/ontowiki.log"
+OWLOG="/var/www/ontowiki/logs/ontowiki.log"
 touch $OWLOG
 chmod a+w $OWLOG
 tail -f $OWLOG
