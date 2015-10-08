e044cd0d1d31dc8d6162c234b0ae43a6bf4d21da
Simeon Ackermann <s.ackermann@mail.de>
2015-10-08 05:57:20 +0200

    Add baseuri as input field

--- d2rq-docker/scripts/dump.sh
+++ d2rq-docker/scripts/dump.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
-OUTPUT=$(/d2rq/dump-rdf /var/www/results/results/mapping.ttl 2>&1)
+MAPPING='/var/www/results/results/mapping.ttl'
+OUTFILE='/var/www/results/results/dump.nt'
+BASE='http://localhost/'
+
+if [ "$#" -eq 1 ] ; then
+	BASE=$1
+fi
+
+echo $BASE
+
+OUTPUT=$(/d2rq/dump-rdf -b ${BASE} -o ${OUTFILE} ${MAPPING} 2>&1)
 
 if [ $? -eq 0 ];then
-  echo $OUTPUT > /var/www/results/results/dump.nt
+  # echo $OUTPUT > /var/www/results/results/dump.nt
   echo "RDF dump created."
 else
   echo "Error: $OUTPUT"
